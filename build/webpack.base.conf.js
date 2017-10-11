5c453701a61e8f84426f35bee03f5654199a1399
zhaihao <1014504021>
2017-10-11 11:36:33 +0800

    迁移到linux开发

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,7 +1,8 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -14,10 +15,9 @@ module.exports = {
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
-    // publicPath: './'
     publicPath: process.env.NODE_ENV === 'production'
-     ? config.build.assetsPublicPath
-     : config.dev.assetsPublicPath
+      ? config.build.assetsPublicPath
+      : config.dev.assetsPublicPath
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'],
